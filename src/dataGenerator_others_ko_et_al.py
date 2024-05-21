8dad6c3437e27997e1d42d7466a2302d5b6d02ff
girinssh <tngus5851@naver.com>
2024-05-21 15:12:03 +0900

    Update dataGenerator_others_ko_et_al.py

--- src/dataGenerator_others_ko_et_al.py
+++ src/dataGenerator_others_ko_et_al.py
@@ -10,8 +10,8 @@ import pickle
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_path',      type=str    ,   default="../")
-parser.add_argument('--output_path',    type=str    ,   default="../processed_data")
+parser.add_argument('--data_path',      type=str    ,   default="./")
+parser.add_argument('--output_path',    type=str    ,   default="./slt_ko_data")
 
 args = parser.parse_args()
 
